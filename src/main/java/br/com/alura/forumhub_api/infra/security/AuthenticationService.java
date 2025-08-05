bfe91530e5ef616b34fa3c84753160ff3304e209
tirsoalc <tirsonetogz@gmail.com>
2025-08-05 13:40:01 -0300

    feat: implement UserDetailsService (AuthenticationService) and UserDetails (UserPrincipal) for authentication

---
+++ src/main/java/br/com/alura/forumhub_api/infra/security/AuthenticationService.java
@@ -0,0 +1,26 @@
+package br.com.alura.forumhub_api.infra.security;
+
+import br.com.alura.forumhub_api.domain.entity.User;
+import br.com.alura.forumhub_api.domain.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+
+        return new UserPrincipal(user);
+    }
+}

a81b9551652deba90f4593f6e08abe39f8564f58
tirsoalc <tirsonetogz@gmail.com>
2025-08-07 17:48:50 -0300

    feat: hide user sensitive information (email) in topic/reply/auth related responses

--- src/main/java/br/com/alura/forumhub_api/api/dto/response/UserResponse.java
+++ src/main/java/br/com/alura/forumhub_api/api/dto/response/UserResponse.java
@@ -4,14 +4,12 @@ import br.com.alura.forumhub_api.domain.entity.User;
 
 public record UserResponse(
         Long id,
-        String name,
-        String email
+        String name
 ) {
     public static UserResponse fromDomain(User user) {
         return new UserResponse(
                 user.getId(),
-                user.getName(),
-                user.getEmail()
+                user.getName()
         );
     }
 }
